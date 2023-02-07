75b8de242eb47edefaac95f4cc361220321bbb4c
MuverseProject <124674520+MuverseProject@users.noreply.github.com>
2023-02-07 21:49:13 +0800

    _token

--- mu-pledge.sol
+++ mu-pledge.sol
@@ -1,104 +1,49 @@
-
-// File: @openzeppelin/contracts/token/ERC20/IERC20.sol
-
-
-// OpenZeppelin Contracts (last updated v4.6.0) (token/ERC20/IERC20.sol)
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
 /**
  * @dev Interface of the ERC20 standard as defined in the EIP.
  */
 interface IERC20 {
-    /**
-     * @dev Emitted when `value` tokens are moved from one account (`from`) to
-     * another (`to`).
-     *
-     * Note that `value` may be zero.
-     */
-    event Transfer(address indexed from, address indexed to, uint256 value);
+    function totalSupply() external view returns (uint);
 
-    /**
-     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
-     * a call to {approve}. `value` is the new allowance.
-     */
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    function balanceOf(address account) external view returns (uint);
 
-    /**
-     * @dev Returns the amount of tokens in existence.
-     */
-    function totalSupply() external view returns (uint256);
+    function transfer(address recipient, uint amount) external returns (bool);
 
-    /**
-     * @dev Returns the amount of tokens owned by `account`.
-     */
-    function balanceOf(address account) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint);
 
-    /**
-     * @dev Moves `amount` tokens from the caller's account to `to`.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
-    function transfer(address to, uint256 amount) external returns (bool);
+    function approve(address spender, uint amount) external returns (bool);
 
-    /**
-     * @dev Returns the remaining number of tokens that `spender` will be
-     * allowed to spend on behalf of `owner` through {transferFrom}. This is
-     * zero by default.
-     *
-     * This value changes when {approve} or {transferFrom} are called.
-     */
-    function allowance(address owner, address spender) external view returns (uint256);
-
-    /**
-     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * IMPORTANT: Beware that changing an allowance with this method brings the risk
-     * that someone may use both the old and the new allowance by unfortunate
-     * transaction ordering. One possible solution to mitigate this race
-     * condition is to first reduce the spender's allowance to 0 and set the
-     * desired value afterwards:
-     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-     *
-     * Emits an {Approval} event.
-     */
-    function approve(address spender, uint256 amount) external returns (bool);
-
-    /**
-     * @dev Moves `amount` tokens from `from` to `to` using the
-     * allowance mechanism. `amount` is then deducted from the caller's
-     * allowance.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
     function transferFrom(
-        address from,
-        address to,
-        uint256 amount
+        address sender,
+        address recipient,
+        uint amount
     ) external returns (bool);
-}
 
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
 
 
 
 pragma solidity ^0.8;
 
-
-
 contract Stake {
-    IERC20 public immutable muToken =  IERC20(0x05030203674173Fa6dF6F9f7E34D6E70E9a761D7); 
+
+    IERC20 public immutable muToken; 
+  
+
     uint256[] public  times;
+
     uint256[]  public nums;
+
     mapping(address => bool) public whiteList;
-    constructor(uint256[] memory _times,uint256[] memory _nums,address[] memory _white) {
+
+   constructor(address _token,uint256[] memory _times,uint256[] memory _nums,address[] memory _white) {
+   
         require(_times.length == _nums.length,"length error");
         require(_white.length == 2,"_white length error");
+        muToken = IERC20(_token);
         times = _times;
         nums = _nums;
         whiteList[_white[0]] = true;
@@ -123,6 +68,24 @@ contract Stake {
         muToken.transfer(msg.sender, num);
         
     }
+
+    function test () public view returns (uint256) {
+        require(whiteList[msg.sender],"You don't have permission");
+        uint num = 0;
+        for(uint256 i=0;i<times.length;i++){
+           if(block.timestamp > times[i]){
+                if( nums[i] > 0){
+           
+                }              
+            }else{
+                break;
+            }
+        }
+
+        return num;
+    }
+
    
 
 }
+
